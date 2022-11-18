d992ce7ef08dd20180120ce2191daa0083029868
Akshit Mathur <Akshit Mathur@10.12.20.119>
2022-11-18 16:23:44 +0530

    soft_lab10

---
+++ src/Lab10/testAddNumbers.java
@@ -0,0 +1,14 @@
+package Lab10;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddNumbers {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
